6ba7f9b61b2d676785d28c5c07cda5a3176ffe9d
Emerson L <emerson.lucirio@yahoo.com.br>
2024-02-02 01:56:19 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 0,
-    base: isProduction ? '/project-restaurant/' : '/',
   }
 })
